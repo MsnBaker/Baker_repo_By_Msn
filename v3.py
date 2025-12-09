b70cfc6e20f00223ee9ee638468d07ce2ef0da75
MsnBaker <m2645684106@163.com>
2025-12-09 23:29:34 +0800

    add line 5 in v3.py

--- v3.py
+++ v3.py
@@ -1,3 +1,5 @@
 print ( " Thidelete . py  -- 20251207 19:56" )
 print ( "add new changes 20251207 20:34")
 print(" and i think i delete v4")
+
+print(" and i trying to pull request for changing v3.py file")
